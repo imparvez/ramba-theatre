7337fe8f0ac97baa9e24b439964e77e073be0b23
Parvez shaikh <parvezshaikhj@gmail.com>
2015-05-31 23:08:28 +0530

    Site completed for desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,29 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-    $('#main-slider').flexslider({
-      animation: "slide",
-  	});
+
+  var windowHeight = $(window).height(),
+      headerHeight = $('header').outerHeight(),
+      footerHeight = $('.wrapper footer').outerHeight();
+
+  $('#main-slider').flexslider({
+    animation: "slide",
+	});
 
   $('#reviews-slider').flexslider({
-      animation: "slide",
-      itemWidth: 200,
-      itemMargin: 1,
-      minItems: 3,
-      maxItems: 9,
-      move: 0,
-  	});
+    animation: "slide",
+    animationLoop: true,
+    itemWidth: 131,
+    itemMargin: 10,
+    minItems: 3,
+    maxItems: 9,
+    move: 0,
+	});
+
+  $('.menu-trigger').on('click', function(){
+    $('.nav-menu').slideToggle(400, function(){
+        $(this).toggleClass('nav-expanded');
+    });
+  });
+
+  // $('main').css('min-height', (windowHeight - headerHeight - footerHeight));
 });
 
